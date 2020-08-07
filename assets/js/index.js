c3b7122bd8b8317a126302fdc1d99e1cbedc8554
Toks Desalu <desalu.developer@gmail.com>
2020-08-07 19:23:39 -0400

    Few revision updated

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,13 @@
 $(document).ready(function () {
-  
+  //var bg = document.body.style.backgroundImage = document.body.style.backgroundImage;
+
+  $(".test").on("click", function () {
+    $("#bg").animate({
+      opacity: 0,
+    }, 2000, function() {
+      window.location.href = "./explore.html";
+    })
+
+    
+  });
 });
